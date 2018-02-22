63be2018797a06f503eec37b674da3088db3e8f4
Rizer <bashinsky.alexey@gmail.com>
2018-02-22 04:34:30 +0300

    css mobile

--- app/index.js
+++ app/index.js
@@ -1,2 +1,21 @@
 import 'styles/style.styl';
 console.log('Hello World1');
+
+let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+const header = document.querySelector('.header');
+
+const setHeaderState = (scrolled) =>  {
+    if(scrolled > 20 && !header.classList.contains('is-moved')){
+        header.classList.add('is-moved');
+    }
+    if(scrolled <=20 && header.classList.contains('is-moved')){
+        header.classList.remove('is-moved');
+    }
+};
+
+setHeaderState(scrolled);
+
+window.addEventListener('scroll', () => {
+    scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    setHeaderState(scrolled);
+});
